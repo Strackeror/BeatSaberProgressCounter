97101f7e778e09039f568ddb318aaed33684aa8d
Kyle1413 <kylet1412@gmail.com>
2018-10-05 18:52:54 -0400

    Cleanup, Adding presets for Positions

--- ProgressCounter/Plugin.cs
+++ ProgressCounter/Plugin.cs
@@ -23,7 +23,10 @@ namespace ProgressCounter
         public static Vector3 scoreCounterPosition = new Vector3(3.25f, 0.5f, 7f);
         public static bool progressTimeLeft = false;
         public static Vector3 progressCounterPosition = new Vector3(0.25f, -2f, 7.5f);
-        public static float progressCounterDecimalPrecision;
+
+        public static int progressCounterDecimalPrecision;
+        public static int progressCounterPositionPreset;
+        public static int progressCounterScorePositionPreset;
 
         public void OnApplicationQuit()
         {
@@ -71,21 +74,21 @@ namespace ProgressCounter
             {
                 progressTimeLeft = ModPrefs.GetBool("BeatSaberProgressCounter", "progressTimeLeft");
             }
+            //Get value for Decimal Precision from modprefs
+            progressCounterDecimalPrecision = ModPrefs.GetInt("BeatSaberProgressCounter", "progressCounterDecimalPrecision", 1, true);
+            limitRange(ref progressCounterDecimalPrecision, 1, 4);
 
-            progressCounterDecimalPrecision = (float)Math.Floor((ModPrefs.GetFloat("BeatSaberProgressCounter", "progressCounterDecimalPrecision", 1, true)));
-            //Force progressCounterDecimalPosition to be from 1-4 if outside range
-            if(progressCounterDecimalPrecision < 1)
-            {
-                progressCounterDecimalPrecision = 1;
-                ModPrefs.SetFloat("BeatSaberProgressCounter", "progressCounterDecimalPrecision", 1);
-            }
-            if(progressCounterDecimalPrecision > 4)
-            {
-                progressCounterDecimalPrecision = 4;
-                ModPrefs.SetFloat("BeatSaberProgressCounter", "progressCounterDecimalPrecision", 4);
-            }
+            //Get Value, Force progressCounterPositionPreset to be within range
+            progressCounterPositionPreset = ModPrefs.GetInt("BeatSaberProgressCounter", "progressCounterPositionPreset", 0, true);
 
+            //Get Value, Force progressCounterScorePositionPreset within range
+            progressCounterScorePositionPreset = ModPrefs.GetInt("BeatSaberProgressCounter", "progressCounterScorePositionPreset", 0, true);
 
+            //Set position of Score Counter if Preset selected
+            setScoreCounterPosition(progressCounterScorePositionPreset);
+
+            //Set modprefs for timer position if Preset Selected
+            setProgressCounterPosition(progressCounterPositionPreset);
 
 
             SceneManager.activeSceneChanged += OnSceneChanged;
@@ -121,5 +124,105 @@ namespace ProgressCounter
         public void OnUpdate()
         {
         }
+
+        public static void setScoreCounterPosition(int value)
+        {
+            switch (value)
+            {
+                case 0: //User
+                    var scoreString = ModPrefs.GetString("BeatSaberProgressCounter", "scorePosition");
+                    var scorePosVals = scoreString.Split(',').Select(f => float.Parse(f, CultureInfo.InvariantCulture)).ToArray();
+                    scoreCounterPosition = new Vector3(scorePosVals[0], scorePosVals[1], scorePosVals[2]);
+                    break;
+
+                case 1: //Default
+                    scoreCounterPosition = new Vector3(3.25f, 0.5f, 7.0f);
+                    break;
+
+                case 2: //Left
+                    scoreCounterPosition = new Vector3(-3.25f, -0.3f, 7.0f);
+                    break;
+
+                default:
+
+                    progressCounterScorePositionPreset = 0;
+
+                    scoreString = ModPrefs.GetString("BeatSaberProgressCounter", "scorePosition");
+                    scorePosVals = scoreString.Split(',').Select(f => float.Parse(f, CultureInfo.InvariantCulture)).ToArray();
+                    scoreCounterPosition = new Vector3(scorePosVals[0], scorePosVals[1], scorePosVals[2]);
+                    break;
+            }
+
+            ModPrefs.SetString("BeatSaberProgressCounter", "scorePosition",
+                     FormattableString.Invariant(
+                    $"{scoreCounterPosition.x:0.00},{scoreCounterPosition.y:0.00},{scoreCounterPosition.z:0.00}"));
+        }
+
+        public static void setProgressCounterPosition(int value)
+        {
+            switch (value)
+            {
+                case 0: //User
+                    var progressString = ModPrefs.GetString("BeatSaberProgressCounter", "progressPosition");
+                    var progressPosVals = progressString.Split(',').Select(f => float.Parse(f, CultureInfo.InvariantCulture)).ToArray();
+                    progressCounterPosition = new Vector3(progressPosVals[0], progressPosVals[1], progressPosVals[2]);
+                    break;
+
+                case 1: //Default
+                    progressCounterPosition = new Vector3(.25f, -2.0f, 7.5f);
+                    break;
+
+                case 2: //Top
+                    progressCounterPosition = new Vector3(.25f, 3.4f, 7.5f);
+                    break;
+
+                case 3: //TopLeft
+                    progressCounterPosition = new Vector3(-3.0f, 3.4f, 7f);
+                    break;
+
+                case 4: //TopRight
+                    progressCounterPosition = new Vector3(3.5f, 3.4f, 7f);
+                    break;
+
+                case 5: //BottomLeft
+                    progressCounterPosition = new Vector3(-3.0f, -1.75f, 7f);
+                    break;
+
+                case 6: //BottomRight
+                    progressCounterPosition = new Vector3(3.5f, -1.6f, 7f);
+                    break;
+
+                default:
+
+                    progressCounterPositionPreset = 0;
+
+                    progressString = ModPrefs.GetString("BeatSaberProgressCounter", "progressPosition");
+                    progressPosVals = progressString.Split(',').Select(f => float.Parse(f, CultureInfo.InvariantCulture)).ToArray();
+                    progressCounterPosition = new Vector3(progressPosVals[0], progressPosVals[1], progressPosVals[2]);
+                    break;
+            }
+
+            ModPrefs.SetString("BeatSaberProgressCounter", "progressPosition",
+             FormattableString.Invariant(
+           $"{progressCounterPosition.x:0.00},{progressCounterPosition.y:0.00},{progressCounterPosition.z:0.00}"));
+
+        }
+
+        public void limitRange(ref int value, int min, int max)
+        {
+            if (value > max)
+            {
+                value = max;
+            }
+
+            else if (value < min)
+            {
+                value = min;
+            }
+
+        }
+
+
+
     }
 }
